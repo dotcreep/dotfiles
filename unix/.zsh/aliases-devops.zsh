778c708e91bfa0a145112b4d4383ba3e5a66534b
dotcreep <git@dotcreep.com>
2023-05-23 13:12:00 +0700

    Update aliases

--- unix/.zsh/aliases-devops.zsh
+++ unix/.zsh/aliases-devops.zsh
@@ -1,5 +1,5 @@
 # This command is used a LOT both below and in daily life
-alias k=kubectl
+alias k='kubectl'
 
 # Execute a kubectl command against all namespaces
 alias kca='_kca(){ kubectl "$@" --all-namespaces;  unset -f _kca; }; _kca'
