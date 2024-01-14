f9fd3a06ce1a0689ebf46098faad4505dd997f4b
dotcreep <git@dotcreep.com>
2024-01-14 09:30:56 +0700

    Optimize uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,10 +1,20 @@
 function uninstall_dotfiles(){
   echo "NOTICE: Your custom aliases still existed in $HOME/.zsh-custom"
-  [[ -f "$HOME/.zshrc" ]] && rm -rf $HOME/.zshrc
-  [[ -f "$HOME/.zsh" ]] && rm -rf $HOME/.zsh
-  [[ -f "$HOME/.zsh-plugin" ]] && rm -rf $HOME/.zsh-plugin
-  [[ -f "$HOME/.config/nvim" ]] && rm -rf $HOME/.config/nvim
-  [[ -f "$HOME/.termux/font-backup.ttf" ]] && mv $HOME/.termux/font-backup.ttf $HOME/.termux/font.ttf
+  if [[ -f "$HOME/.zshrc" ]]; then
+    rm -rf $HOME/.zshrc
+  fi
+  if [[ -f "$HOME/.zsh" ]]; then
+    rm -rf $HOME/.zsh
+  fi
+  if [[ -f "$HOME/.zsh-plugin" ]]; then
+    rm -rf $HOME/.zsh-plugin
+  fi
+  if [[ -f "$HOME/.config/nvim" ]]; then
+    rm -rf $HOME/.config/nvim
+  fi
+  if [[ -f "$HOME/.termux/font-backup.ttf" ]]; then
+    mv $HOME/.termux/font-backup.ttf $HOME/.termux/font.ttf
+  fi
   if [[ $? -ne 0 ]]; then
     echo "ERROR: Failed uninstall"
     return 1
